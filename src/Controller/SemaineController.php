5a47c3fd22dc924c66b5085a477cb99654de48f4
Merlin <merlin31770@gmail.com>
2023-05-03 12:57:47 +0200

    Ajout de deux fonction, filmproposes, nextProposeurs

--- src/Controller/SemaineController.php
+++ src/Controller/SemaineController.php
@@ -3,14 +3,19 @@
 namespace App\Controller;
 
 
+use App\Entity\Semaine;
 use App\Service\PrintSemaine;
 use App\Service\SemaineService;
+use App\Controller\SemaineController;
 use App\Repository\SemaineRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\PropositionRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Serializer\SerializerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
 
 class SemaineController extends AbstractController
 {
@@ -38,11 +43,38 @@ class SemaineController extends AbstractController
     }
 
     #[Route('/filmsProposes/{id_semaine}', name: 'filmsProposes', methods: ['GET'])]
-    public function filmsProposes(int $id_semaine, SemaineService $semaineService, SerializerInterface $serializer): JsonResponse
+    public function filmsProposes(int $id_semaine, PropositionRepository $propositionRepository, SerializerInterface $serializer): JsonResponse
     {
-        $filmList = $semaineService->getFilmsProposes($id_semaine);
-        $jsonFilmlist = $serializer->serialize($filmList, 'json');
-        return new JsonResponse ($jsonFilmlist, Response::HTTP_OK, [], true);
+        $filmsProposes = $propositionRepository->findBySemaine($id_semaine);
+        $jsonFilmProposes = $serializer->serialize($filmsProposes, 'json');
+        return new JsonResponse ($jsonFilmProposes, Response::HTTP_OK, [], true);
+
+    }
+
+    #[Route('/nextProposeurs/{id_semaine}', name:'nextProposeurs', methods: ['GET'])]
+    public function nextProposeurs(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        //Récuperer le jour de la semaine $id_semaine
+        $queryBuilder_get_jour = $entityManager->createQueryBuilder();
+        $queryBuilder_get_jour->select('s.jour')
+        ->from(Semaine::class, 's')
+        ->where('s.id = :id')
+        ->setParameter('id', $id_semaine);
+
+        $resultats_jour = $queryBuilder_get_jour->getQuery()->getResult();
+
+        //Récuperer les proposeurs des semaines postérieurs au jour précédent récupéré
+
+        $queryBuilder_get_proposeurs = $entityManager->createQueryBuilder();
+        $queryBuilder_get_proposeurs->select('s.proposeur','s.jour')
+        ->from(Semaine::class, 's')
+        ->where('s.jour >= :jour')
+        ->setParameter('jour', $resultats_jour[0]['jour']);
+
+        $resultats_proposeurs = $queryBuilder_get_proposeurs->getQuery()->getResult();
+        $jsonResultatsProposeurs = $serializer->serialize($resultats_proposeurs, 'json');
+
+        return new JsonResponse ($jsonResultatsProposeurs, Response::HTTP_OK, [], true);
 
     }
 }
