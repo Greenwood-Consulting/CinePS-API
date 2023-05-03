90ebf847a594a3db3ec73eebf53e84d8fe9db396
Merlin <merlin31770@gmail.com>
2023-05-03 13:02:35 +0200

    Ajout de deux fonctions votesSemaines, FilmsVictorieux

--- src/Controller/VoteController.php
+++ src/Controller/VoteController.php
@@ -2,9 +2,17 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
+use App\Entity\Vote;
+use App\Entity\Semaine;
+use App\Entity\Proposition;
+use App\Repository\VoteRepository;
+use JMS\Serializer\SerializerInterface;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Repository\PropositionRepository;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class VoteController extends AbstractController
 {
@@ -30,13 +38,43 @@ class VoteController extends AbstractController
         return new JsonResponse(["error" => "Not Found"], 404);
     }
 
-    #[Route('/api/propositions/{id}', name: 'deleteProposition', methods: ['DELETE'])]
-    public function deleteProposition(Proposition $proposition, EntityManagerInterface $em): JsonResponse
-    {
-        $em->remove($proposition);
-        $em->flush();
 
-        return new JsonResponse(null, Response::HTTP_NO_CONTENT);
+    //Afficher les votes de la semaine $id_semaine
+    #[Route('/votes/{id_semaine}', name:'VotesSemaine', methods: ['GET'])]
+    public function voteSemaine(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        //Récupérer les votes des proposeurs de $id_semaine
+        $queryBuilder_get_votes = $entityManager->createQueryBuilder();
+        $queryBuilder_get_votes->select('v')
+        ->from(Vote::class, 'v')
+        ->where('v.semaine = :semaine')
+        ->setParameter('semaine', $id_semaine);
+
+        $resultats_votes = $queryBuilder_get_votes->getQuery()->getResult();
+        $jsonResultatsVotes = $serializer->serialize($resultats_votes, 'json');
+
+        return new JsonResponse ($jsonResultatsVotes, Response::HTTP_OK, [], true);
     }
+
+    //Afficher les film victorieux de la semaine id_semaine
+    #[Route('/filmVictorieux/{id_semaine}', name:'FilmVictorieux', methods: ['GET'])]
+    public function filmVictorieux(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        //Récupérer le film de la semaine qui a le score le plus élevé
+        $queryBuilder_get_film_victorieux = $entityManager->createQueryBuilder();
+        $queryBuilder_get_film_victorieux->select('p')
+        ->from(Proposition::class, 'p')
+        ->orderBy('p.score', 'DESC')
+        ->setMaxResults(1)
+        ->where('p.semaine = :semaine')
+        ->setParameter('semaine', $id_semaine);
+
+        $film_victorieux = $queryBuilder_get_film_victorieux->getQuery()->getResult();
+        $jsonFilmVictorieux = $serializer->serialize($film_victorieux, 'json');
+
+        return new JsonResponse ($jsonFilmVictorieux, Response::HTTP_OK, [], true);
+    }
+
+
 
 }
