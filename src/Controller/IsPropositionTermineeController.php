ac472d5c34a3b199006cf87a9b741b40c5105a30
Merlin <merlin31770@gmail.com>
2023-05-19 12:09:53 +0200

    Ajout de l'etat isVoteTerminé

--- src/Controller/IsPropositionTermineeController.php
+++ src/Controller/IsPropositionTermineeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\AVote;
+use App\Entity\Membre;
 use App\Entity\Semaine;
 use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
@@ -13,6 +15,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class IsPropositionTermineeController extends AbstractController
 {
+
+    //Vérifie si la proposition_terminée = à 0 où 1
     #[Route('/isPropositionTerminee/{id_semaine}', name: 'app_is_proposition_terminee')]
     public function isPropositionTerminee(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
     {
@@ -23,8 +27,6 @@ class IsPropositionTermineeController extends AbstractController
         ->where('s.id = :id')
         ->setParameter('id', $id_semaine);
 
-
-        
 
         $resultat_proposition_terminee = $queryBuilder_get_jour->getQuery()->getResult();
 
@@ -37,14 +39,35 @@ class IsPropositionTermineeController extends AbstractController
 
 
         return new JsonResponse ($resultat_controller, Response::HTTP_OK, [], true);
+    }
 
+    //Indique si le vote pour la semaine $id_semaine est terminée
+    #[Route('/isVoteTermine/{id_semaine}', name: 'app_is_proposition_terminee')]
+    public function isVoteTermineCetteSemaine(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        ///Récupère le nombre de votants de la semaine $id_semaine
+        $queryBuilder_get_nb_votants_semaine = $entityManager->createQueryBuilder();
+        $queryBuilder_get_nb_votants_semaine->select('COUNT(a.votant)')
+        ->from(AVote::class, 'a')
+        ->where('a.semaine = :id')
+        ->setParameter('id', $id_semaine);
+
+        $resultat_nb_votants_semaine_termine = $queryBuilder_get_nb_votants_semaine->getQuery()->getResult();
+        $get_int_resultat_nb_votants_semaine_termine = $resultat_nb_votants_semaine_termine[0][1];
+
+
+        //Récupere le nombre de membres - 1
+        $queryBuilder_get_membre = $entityManager->createQueryBuilder();
+        $queryBuilder_get_membre->select('COUNT(m.id)')
+        ->from(Membre::class, 'm');
+
+        $resultat_count_membre = $queryBuilder_get_membre->getQuery()->getResult();
+        $get_int_resultat_count_membre = $resultat_count_membre[0][1];
+
+        $vote_termine_cette_semaine = ($get_int_resultat_nb_votants_semaine_termine == ($get_int_resultat_count_membre -1));
         
-        // // TODO: Construire une réponse identique lorsque qu'il n'y a pas de semaine existante
-        // if(count($resultat_proposition_terminee) === 0){
-        //     $resultat_controller = new JsonResponse(json_encode(['proposition_termine'=> 0]));
-        // }else{
-        //      
-        // }
-        // 
+        $jsonVote_termine_cette_semaine = $serializer->serialize($vote_termine_cette_semaine, 'json');
+
+        return new JsonResponse ($jsonVote_termine_cette_semaine, Response::HTTP_OK, [], true);
     }
 }
