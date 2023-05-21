81dbd4eee41fd8852dbff2f2d998748af898798d
bbo <bbo-github@gc2.fr>
2023-05-22 00:48:46 +0200

    Ajout service anciennesSemaines qui retourne toutes les semaines précédant la semaine courante (nécessaire pour afficher l'historique des votes)

--- src/Controller/SemaineController.php
+++ src/Controller/SemaineController.php
@@ -80,6 +80,47 @@ class SemaineController extends AbstractController
         return new JsonResponse ($json_id_current_semaine, Response::HTTP_OK, [], true);
     }
 
+        // Retourne l'onjet de la semaine en cours
+        #[Route('/api/anciennesSemaines', name: 'anciennesSemaines', methods: ['GET'])]
+        public function getAnciennesSemaines(SerializerInterface $serializer, EntityManagerInterface $entityManager, SemaineRepository $semaineRepository): JsonResponse
+        {
+            // TODO Factoriser le calcul du vendredi courant ?
+            // Date du jour
+            $curdate=new DateTime();
+    
+            // calcul de la date de fin de la période de vote
+            $fin_periode_vote = new DateTime("Fri 14:00");
+            $fin_periode_vote = $fin_periode_vote->format('Y-m-d H:i:s');
+    
+            // conversion de la date de fin en timestamp
+            $deadline_vote = strtotime($fin_periode_vote);
+            $deadline_vote = $deadline_vote*1000;
+    
+            // Get vendredi id_current_semaine
+            if ($curdate->format('D')=="Fri"){ // Si nous sommes vendredi, alors id_current_semaine est défini par ce vendredi
+                $friday_current_semaine = $curdate->format('Y-m-d');
+            } else { // Sinon id_current_semaine est défini par vendredi prochain
+                $friday_current_semaine = $curdate->modify('next friday')->format('Y-m-d');
+            }
+    
+            //Récupère la propositionTerminé de id_semaine
+            $queryBuilder_get_id_current_semaine = $entityManager->createQueryBuilder();
+            $queryBuilder_get_id_current_semaine->select('s')
+            ->from(Semaine::class, 's')
+            ->where('s.jour < :jour')
+            ->orderBy('s.jour', 'DESC')
+            ->setParameter('jour', $friday_current_semaine);
+    
+            $result_current_semaine = $queryBuilder_get_id_current_semaine->getQuery()->getResult();
+            
+            if($result_current_semaine) {
+                $jsonProposition = $serializer->serialize($result_current_semaine, 'json');
+                return new JsonResponse($jsonProposition, Response::HTTP_OK, [], true);
+            }
+            return new JsonResponse(["error" => "Not Found"], 404);
+        }
+    
+
     #[Route('/filmsProposes/{id_semaine}', name: 'filmsProposes', methods: ['GET'])]
     public function filmsProposes(int $id_semaine, PropositionRepository $propositionRepository, SerializerInterface $serializer): JsonResponse
     {
