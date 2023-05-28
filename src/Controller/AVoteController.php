0faeb2b4b36d70f74a6668d0e1b8a6c3e7f59a7d
bbo <bbo-github@gc2.fr>
2023-05-28 19:32:33 +0200

    Ajout d'un entrypoint /aVoteCurrentSemaine/{id_membre} qui indique si le membre en paramètre a voté pour la semaine en cours ou non

--- src/Controller/AVoteController.php
+++ src/Controller/AVoteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\AVote;
+use App\Service\CurrentSemaine;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -24,8 +25,34 @@ class AVoteController extends AbstractController
         ->setParameter('semaine', $id_semaine);
 
         $membre_votant = $queryBuilder_get_membre_votant->getQuery()->getResult();
-        $jsonMembreVotant = $serializer->serialize($membre_votant, 'json');
+        $jsonMembreVotant = $serializer->serialize($membre_votant, 'json', ['groups' => 'getPropositions']);
 
         return new JsonResponse ($jsonMembreVotant, Response::HTTP_OK, [], true);
     }
+    
+    // Indique si l'utilisateur a voté pour alse amine en cours
+    #[Route('/aVoteCurrentSemaine/{id_membre}', name:'AVoteCurrentSemaine', methods: ['GET'])]
+    public function aVoteCurrentSemaine(CurrentSemaine $currentSemaine, int $id_membre, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $id_current_semaine = $currentSemaine->getIdCurrentSemaine($entityManager);
+
+        $queryBuilder_get_aVote = $entityManager->createQueryBuilder();
+        $queryBuilder_get_aVote->select('av.id')
+        ->from(AVote::class, 'av')
+        ->where('av.semaine = :semaine')
+        ->andWhere('av.votant = :votant')
+        ->setParameters(array('semaine' => $id_current_semaine, 'votant' => $id_membre));
+
+        $resultats_aVote = $queryBuilder_get_aVote->getQuery()->getResult();
+
+
+        if (empty($resultats_aVote)) {
+            $result = $serializer->serialize(false, 'json');
+        } else {
+            $result = $serializer->serialize(true, 'json');
+        }
+        return new JsonResponse ($result, Response::HTTP_OK, [], true);
+
+    }
+
 }
