85163e9affe2184cb8979166f19cb8c938ad7fd3
bbo <bbo-github@gc2.fr>
2023-05-29 01:58:43 +0200

    suppression de la route POST /api/films inutilisée

--- src/Controller/FilmController.php
+++ src/Controller/FilmController.php
@@ -3,53 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Film;
-use App\Service\PrintFilm;
-use App\Service\HelloWorld;
-use App\Repository\FilmRepository;
 use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Contracts\Cache\ItemInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Contracts\Cache\TagAwareCacheInterface;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use  Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FilmController extends AbstractController
 {
-    
-    //Permet à l'admin d'ajouter un film
-    #[Route('/api/films', name:"createFilm", methods: ['POST'])]
-    #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour proposer un film')]
-    public function createFilm(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator, ValidatorInterface $validator): JsonResponse 
-    {
-
-        $film = $serializer->deserialize($request->getContent(), Film::class, 'json');
-
-        // On vérifie les erreurs
-        $errors = $validator->validate($film);
-
-        if ($errors->count() > 0) {
-            return new JsonResponse($serializer->serialize($errors, 'json'), JsonResponse::HTTP_BAD_REQUEST, [], true);
-        }
-
-
-
-        $em->persist($film);
-        $em->flush();
-
-        $jsonFilm = $serializer->serialize($film, 'json');
-        
-        $location = $urlGenerator->generate('detailFilm', ['id' => $film->getId()], UrlGeneratorInterface::ABSOLUTE_URL);
-
-        return new JsonResponse($jsonFilm, Response::HTTP_CREATED, ["Location" => $location], true);
-   }
-
-
     //Permet à l'admin de modifier les films
     #[Route('/api/films/{id}', name:"updateFilm", methods:['PUT'])]
     #[IsGranted('ROLE_ADMIN', message: 'Vous n\'avez pas les droits suffisants pour éditer un film')]
