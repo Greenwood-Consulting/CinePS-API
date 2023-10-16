c0c5dd136ae087fd1116ec085692ab6405125d48
Merlin <merlin31770@gmail.com>
2023-10-16 16:41:18 +0200

    Ajout d'une fonction qui calcule le nombre de propostions par proposeur

--- src/Controller/GetProposeurController.php
+++ src/Controller/GetProposeurController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Semaine;
+use App\Repository\MembreRepository;
 use JMS\Serializer\SerializerInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
@@ -50,5 +51,24 @@ class GetProposeurController extends AbstractController
         if(isset($resultat_proposeur_semaine)){
             return new JsonResponse ($jsonResultatsProposeurSemaine, Response::HTTP_OK, [], true);
         }
+    }
+
+    //Récupère le nombre de proposeur 
+    #[Route('/api/getNbPropositionsParProposeur', name: 'app_get_proposeur')]
+    public function getCountProposeurSemaine(EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $queryBuilder_get_nb_propositions_par_proposeur = $entityManager->createQueryBuilder();
+        $queryBuilder_get_nb_propositions_par_proposeur->select('p.Nom AS proposeur','COUNT(s.id) AS nb_semaines')
+        ->from(Semaine::class, 's')
+        ->innerJoin('s.proposeur' ,'p')
+        ->groupBy('s.proposeur');
+        
+        $Resultat_nb_propositions_par_proposeur = $queryBuilder_get_nb_propositions_par_proposeur->getQuery()->getResult();
+        $jsonResultatNbPropositionParProposeur = $serializer->serialize($Resultat_nb_propositions_par_proposeur, 'json');
+        
+        if(isset($jsonResultatNbPropositionParProposeur))
+        return new JsonResponse ($jsonResultatNbPropositionParProposeur, Response::HTTP_OK, [], true);
+
+        
     }
 }
