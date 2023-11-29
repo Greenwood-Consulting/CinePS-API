fbf59faa4047c475b12ba0f2f040498761476dd2
Merlin <merlin31770@gmail.com>
2023-11-29 17:45:51 +0100

    Suppression de la fonction membreVotant

--- src/Controller/AVoteController.php
+++ src/Controller/AVoteController.php
@@ -13,21 +13,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AVoteController extends AbstractController
 {
-    //Récupérer les membre ayant voté
-    #[Route('/api/membreVotant/{id_semaine}', name:'membreVotant', methods: ['GET'])]
-    public function membreVotant(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
-    {
-    $queryBuilder_get_membre_votant = $entityManager->createQueryBuilder();
-    $queryBuilder_get_membre_votant->select('a')
-    ->from(AVote::class, 'a')
-    ->where('a.semaine = :semaine')
-    ->setParameter('semaine', $id_semaine);
-
-    $membre_votant = $queryBuilder_get_membre_votant->getQuery()->getResult();
-    $jsonMembreVotant = $serializer->serialize($membre_votant, 'json', ['groups' => 'getPropositions']);
-
-    return new JsonResponse ($jsonMembreVotant, Response::HTTP_OK, [], true);
-    }
+    
     // Indique si l'utilisateur a voté pour alse amine en cours
     #[Route('/api/aVoteCurrentSemaine/{id_membre}', name:'AVoteCurrentSemaine', methods: ['GET'])]
     public function aVoteCurrentSemaine(CurrentSemaine $currentSemaine, int $id_membre, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
