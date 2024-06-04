0105a4d5b3d199c4dbcfc411a2e65fe114df53a2
bboisvert <bbo-github@gc2.fr>
2024-06-04 17:38:25 +0200

    Suppression du endpoint '/api/isPropositionTerminee/{id_semaine}' car désormais inutile car on utilise currentSemaine qui contient cette information

--- src/Controller/IsPropositionTermineeController.php
+++ src/Controller/IsPropositionTermineeController.php
@@ -16,31 +16,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class IsPropositionTermineeController extends AbstractController
 {
 
-    //Vérifie si la proposition_terminée = à 0 où 1
-    #[Route('/api/isPropositionTerminee/{id_semaine}', name: 'app_is_proposition_terminee')]
-    public function isPropositionTerminee(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
-    {
-        //Récupère la propositionTerminé de id_semaine
-        $queryBuilder_get_jour = $entityManager->createQueryBuilder();
-        $queryBuilder_get_jour->select('s.id, COALESCE(s.proposition_termine, 0) as proposition_termine')
-        ->from(Semaine::class, 's')
-        ->where('s.id = :id')
-        ->setParameter('id', $id_semaine);
-
-
-        $resultat_proposition_terminee = $queryBuilder_get_jour->getQuery()->getResult();
-
-        if (empty($resultat_proposition_terminee)) {
-            $resultat_controller = $serializer->serialize([0 => ['proposition_termine' => 0]], 'json');
-        } else {
-            $jsonResultatsPropositionTerminee = $serializer->serialize($resultat_proposition_terminee, 'json');
-            $resultat_controller = $jsonResultatsPropositionTerminee;
-        }
-
-
-        return new JsonResponse ($resultat_controller, Response::HTTP_OK, [], true);
-    }
-
     //Indique si le vote pour la semaine $id_semaine est terminée
     #[Route('/api/isVoteTermine/{id_semaine}', name: 'is_vote_termine')]
     public function isVoteTermineCetteSemaine(int $id_semaine, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
