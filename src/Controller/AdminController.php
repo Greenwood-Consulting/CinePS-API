fef35b22fb53fb3b270491403cdc6bc6383692e3
bboisvert <bbo-github@gc2.fr>
2024-06-04 17:28:04 +0200

    Suppression du controleur '/api/admin' qui est à la fois obsolète, non utilisé et bugué

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -22,24 +22,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class AdminController extends AbstractController
 {
-    #[Route('/api/admin', name: 'app_admin', methods: ['POST'])]
-    public function nextProposeurs(Request $request, EntityManagerInterface $em, Semaine $semaine, SerializerInterface $serializer,): JsonResponse
-    {
-        $array_request = json_decode($request->getContent(), true);
-
-        $semaine = new Semaine();
-        $semaine->setProposeur($array_request['prochain_proposeur']);
-        $semaine->setJour($array_request['date']);
-        $semaine->setPropositionTermine(0);
-        $semaine->setTheme("");
-
-        $em->persist($semaine);
-        $em->flush();
-
-        $jsonProposeurs = $serializer->serialize($semaine, 'json', ['groups' => 'getProposeurs']); 
-        return new JsonResponse($jsonProposeurs, Response::HTTP_CREATED, [], true);
-    }
-
     #[Route('/api/newmembre', name:"createMembre", methods: ['POST'])]
     public function createMembre(Request $request, SerializerInterface $serializer, EntityManagerInterface $em, UrlGeneratorInterface $urlGenerator): JsonResponse 
     {
