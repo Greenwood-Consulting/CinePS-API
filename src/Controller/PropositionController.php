f402d3c4e852f593c58894377cfb4f8b5d5b1e71
Merlin <merlin31770@gmail.com>
2024-08-30 16:07:34 +0200

    WIP: seconde chance les propositions perdantes sont retournées correctement

--- src/Controller/PropositionController.php
+++ src/Controller/PropositionController.php
@@ -6,7 +6,9 @@ use DateTime;
 use App\Entity\Film;
 use App\Entity\Semaine;
 use App\Entity\Proposition;
+use Psr\Log\LoggerInterface;
 use App\Service\CurrentSemaine;
+use App\Repository\FilmRepository;
 use App\Repository\SemaineRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\PropositionRepository;
@@ -43,6 +45,81 @@ class PropositionController extends AbstractController
 
         $jsonProposition = $serializer->serialize($proposition, 'json', ['groups' => 'getPropositions']); 
         return new JsonResponse($jsonProposition, Response::HTTP_CREATED, [], true);
+    }
+
+
+    #[Route('/api/PropositionPerdante/{proposeur_id}', name: 'proposition_perdante')]
+    public function getPropositionPerdante($proposeur_id, Request $request, SemaineRepository $semaineRepository, EntityManagerInterface $entityManager, SerializerInterface $serializer): JsonResponse
+    {
+        $proposition_perdante = [];
+
+    // Récupérer toutes les semaines pour le proposeur donné
+    $semaines = $semaineRepository->findBy(['proposeur' => $proposeur_id]);
+
+    foreach ($semaines as $semaine) {
+        $id_semaine = $semaine->getId();
+
+        // Sous-requête pour obtenir le score le plus élevé
+        $subQuery = $entityManager->createQueryBuilder()
+            ->select('MAX(p2.score)')
+            ->from(Proposition::class, 'p2')
+            ->leftJoin('p2.semaine', 's2')
+            ->where('s2.id = :id');
+
+        // Requête principale pour récupérer les films sauf celui avec le score le plus élevé
+        $queryBuilder_get_proposition = $entityManager->createQueryBuilder();
+        $queryBuilder_get_proposition->select('p')
+            ->from(Proposition::class, 'p')
+            ->leftJoin('p.semaine', 's')
+            ->where('s.id = :id')
+            ->andWhere('p.score < (' . $subQuery->getDQL() . ')')
+            ->setParameter('id', $id_semaine);
+
+        $get_proposition = $queryBuilder_get_proposition->getQuery()->getResult();
+
+        if (empty($get_proposition)) {
+            $titre_film = $request->request->get('titre_film');
+            $sortie_film = $request->request->get('sortie_film');
+            $lien_imdb = $request->request->get('imdb_film');
+
+            // Si le champ titre_film est null ou vide, passer à l'itération suivante
+            if ($titre_film === null || $titre_film === '') {
+                continue;
+            }
+
+            // Créer un nouvel objet Film
+            $film = new Film();
+            $film->setTitre($titre_film);
+            $film->setDate(new \DateTime());
+            $film->setSortieFilm($sortie_film);
+            $film->setImdb($lien_imdb);
+            
+            // Créer une nouvelle proposition
+            $proposition = new Proposition();
+            $proposition->setSemaine($semaine);
+            $proposition->setFilm($film);
+            $proposition->setScore(36);
+            
+            $entityManager->persist($film);
+            $entityManager->persist($proposition);
+            $entityManager->flush();
+            
+            // Ajouter la nouvelle proposition à $proposition_perdante
+            $proposition_perdante[] = $proposition;
+        } else {
+            // Mélanger les propositions existantes et les ajouter à $proposition_perdante
+            shuffle($get_proposition);
+            $proposition_perdante = array_merge($proposition_perdante, $get_proposition);
+        }
+    }
+
+    // Mélanger toutes les propositions perdantes et en retourner 5 aléatoirement
+    shuffle($proposition_perdante);
+    $random_proposition_perdante = array_slice($proposition_perdante, 0, 5);
+
+    $jsonProposition = $serializer->serialize($random_proposition_perdante, 'json', ['groups' => 'getPropositions']);
+
+    return new JsonResponse($jsonProposition, Response::HTTP_OK, [], true);
     }
 
 }
